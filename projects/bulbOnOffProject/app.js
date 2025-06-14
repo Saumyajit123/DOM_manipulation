6568676e8abba2a9e186598c0398528d1ed9e379
SAUMYAJIT <saumyajit987@gmail.com>
2025-06-14 12:39:44 +0530

    Project Competed

--- projects/bulbOnOffProject/app.js
+++ projects/bulbOnOffProject/app.js
@@ -1,16 +1,16 @@
 const btn = document.querySelector("#btn");
 const image = document.querySelector("#on");
 
-btn.addEventListener('click', change)
-function change() {
-    if(btn.textContent.includes('on')){
-        image.src = "BulbOn.png"
+btn.addEventListener('click', ()=>{
+    // console.log(image.src);
+    if(btn.innerHTML.includes('On')){
+        image.src = "./assets/BulbOn.png"
         btn.style.backgroundColor = "green"
-        btn.textContent = "Turn Off"
+        btn.innerHTML = "Turn Off"
     }else{
-        image.src = "BulbOff.png"
+        image.src = "./assets/BulbOff.png"
         btn.style.backgroundColor = "yellow"
-        btn.textContent = "Turn On"       
+        btn.innerHTML = "Turn On"       
     }
-}
+});
 
